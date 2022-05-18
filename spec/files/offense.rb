d0a0b1077f95a314055f86e3bad4bf267fcd2ba3
fargelus <ddraudred@gmail.com>
2022-05-18 13:52:28 +0300

    Fasterer output case in spec

--- spec/files/offense.rb
+++
@@ -1,9 +0,0 @@
-for number in [*1..100] do
-  number
-end
-
-# don't catch methods named for
-[].for do
-end
-
-[].shuffle.first
--- spec/pronto/fasterer_spec.rb
+++ spec/pronto/fasterer_spec.rb
@@ -1,7 +1,35 @@
 # frozen_string_literal: true
 
+require "open3"
+
 RSpec.describe Pronto::Fasterer do
-  it "has a version number" do
-    expect(Pronto::FastererVersion::VERSION).not_to be nil
+  let(:pronto_cmd) { "pronto run --unstaged -r fasterer" }
+  let(:pronto_output) { Open3.capture2(pronto_cmd).first }
+  let(:file_path) { File.join(Dir.pwd, "spec", "files", "offense.rb") }
+  let(:file_content) { File.read(file_path) }
+
+  context "when file didn't modified" do
+    it "hasn't got any output" do
+      expect(pronto_output.empty?).to be true
+    end
+  end
+
+  context "when file modified" do
+    let(:analyzer) { ::Fasterer::Analyzer.new(file_path) }
+    let(:offense) do
+      analyzer.errors[:shuffle_first_vs_sample].first.explanation
+    end
+
+    before do
+      File.write(file_path, "[].shuffle.first")
+      analyzer.scan
+    end
+
+    after { File.write(file_path, "") }
+
+    it "has same output as fasterer" do
+      expect(pronto_output.empty?).to be false
+      expect(pronto_output).to match offense
+    end
   end
 end
